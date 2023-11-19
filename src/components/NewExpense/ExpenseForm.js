2cb7858d58e9a913ab3c24109912aa987e4bf44d
IgorGazineo <gaumgazineo@hotmail.com>
2023-11-19 06:42:51 -0300

    criando funções para capturar os dados dos inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,68 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const inputChangeHandler = (ident, value) => {
+    if (ident === "title") {
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredTitle: value,
+        };
+      });
+    } else if (ident === "amount") {
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredAmount: value,
+        };
+      });
+    } else {
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredDate: value,
+        };
+      });
+    }
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => inputChangeHandler("title", e.target.value)}
+          />
+          <p>Test: {userInput.enteredTitle}</p>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={(e) => inputChangeHandler("amount", e.target.value)}
+          />
+          <p>Test: {userInput.enteredAmount}</p>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={(e) => inputChangeHandler("date", e.target.value)}
+          />
+          <p>Test: {userInput.enteredDate}</p>
         </div>
       </div>
       <div className="new-expense__actions">
