a91862bd5ac4b26014aad1517625dab497df104b
IgorGazineo <gaumgazineo@hotmail.com>
2023-11-24 07:13:46 -0300

    deixando o filtro dinâmico

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,14 +9,20 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
