372e2137515a95e52edc81034a71baffcd18b936
IgorGazineo <gaumgazineo@hotmail.com>
2023-11-24 07:41:04 -0300

    adicionando funcionalidade que mostra mensagem para ano que não tenha despesa

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,20 +14,26 @@ function Expenses(props) {
     (item) => item.date.getFullYear().toString() === filteredYear
   );
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 }
